2efe98d86630afdd8960c752842dd1dd8177a82b
Kimberly Lazarski <klazarsk@redhat.com>
2026-03-04 18:01:13 -0500

    modified:   src/gnu3d.dem
    Refactored:
    1. Fixed compatibility issue with newer gnuplot versions
    2. Renamed some of the reports to longer, more descriptive names
    3. Refactored graph generation by consolidating duplicated code
       into a loop iterating over all of the data sets
    
    new file:   src/gnu3d.dem.old
    
    This is the old script.
    
    new file:   symboliclinks.sh
    Temporary script for creating symbolic links under /usr/bin

---
+++ symboliclinks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s /opt/iozone/bin/Generate_Graphs /usr/bin/Generate_Graphs
+ln -s /opt/iozone/bin/iozone /usr/bin/iozone
+ln -s /opt/iozone/bin/gengnuplot.sh /usr/bin/gengnuplot.sh
+ln -s /opt/iozone/bin/fileop /usr/bin/fileop
